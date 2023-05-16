2b7b691f1a393b85c92aab89f4ea7a19916b9c39
nkverechsh <vereshagin-n@mail.ru>
2023-05-16 22:40:30 +0300

    js 1-10 tasks (hw 2.2)

--- hw1/js/script.js
+++
@@ -1,7 +0,0 @@
-// alert('Привет;)');
-// confirm('согласен создать конфирм');
-// prompt('сколька вам лет');
-let age = 10;
-age = prompt('Сколько вам лет');
-let productPrice = prompt('Цена товара');
-alert(productPrice);
